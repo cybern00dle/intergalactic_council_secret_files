0ba72c16194769b54b477883ac8a7ed21f15cc13
cybern00dle <104030372+cybern00dle@users.noreply.github.com>
2022-05-22 23:45:00 +0300

    концовки и другие изменения
    - добавлен вызов функции концовок
    - изменены пути к файлам актов
    - мини-изменения в выводимом тексте

--- тест класса персонажей.py
+++ тест класса персонажей.py
@@ -2,6 +2,7 @@ from lib.character import Character
 import lib.scenes as sc
 #девочки, не забываем импортировать сюда своих персонажей, а также добавлять их в два списка ниже
 from lib.characters import Captain, Emo_Janitor, The_Lost_Student, Sergeant_Peepers, Safety_Belt, The_clone, Talking_pie, The_lost_child, Wicket, Predatory_plant, Projector, Red_cat, Printer_station, Seagull_Charmer, Zhenya, FGN_student, Booklets, Cleaner, Timur, Strange_group
+from lib.boss import Boss
 
 #создаем словарь с характеристиками игрока. считаем, что в начале он не мертв, и у него одно очко, список артефактов пуст
 player = {'close_to_end_index': 1, 'player_is_dead': 0, 'artefacts' : []}
@@ -14,16 +15,16 @@ while answer != '1':
     answer = input('-> ').lower().strip()
 
 #создаем 4 списка с персонажами, по одному на акт. дальше пойдем в цикле по элементам списка. шестым элементом списка указываем текст перед актом.
-f = open('texts\\acts\\act1.txt')
+f = open('lib\\texts\\acts\\act1.txt')
 act1_text = f.read()
 f.close()
-f1 = open('texts\\acts\\act2.txt')
+f1 = open('lib\\texts\\acts\\act2.txt')
 act2_text = f1.read()
 f1.close()
-f2 = open('texts\\acts\\act3.txt')
+f2 = open('lib\\texts\\acts\\act3.txt')
 act3_text = f2.read()
 f2.close()
-f3 = open('texts\\acts\\act4.txt')
+f3 = open('lib\\texts\\acts\\act4.txt')
 act4_text = f3.read()
 f3.close()
 character_list_act1 = [Captain(), Emo_Janitor(), The_Lost_Student(), Sergeant_Peepers(), Safety_Belt(), act1_text]
@@ -62,11 +63,15 @@ def artefacts_collection(player):
         final_artefacts.remove('technical_artifact_2')
     if 'technical_artifact_1' in final_artefacts:
         final_artefacts.remove('technical_artifact_1')
-    i = input('Кажется, все разошлись по домам. В вышке никого не осталось. Вы решаете собраться с мыслями и замечаете, что ваша сумка стала очень тяжелой.\n\n1. Открыть сумку и посмотреть, что внутри\n2. Выбросить сумку.\n-> ').lower().strip()
+    i = input('''* Кажется, все разошлись по домам. В Вышке никого не осталось. Вы решаете собраться с мыслями и замечаете, что Ваша сумка стала очень тяжелой.
+1. Открыть сумку и посмотреть, что внутри.
+2. Выбросить сумку.
+-> ''').lower().strip()
     if i == '1' and final_artefacts != []:
-        print('в вашей сумке вы находите: ', final_artefacts, '\nСохраните на будущее. Вдруг пригодится.')
+        print('* В Вашей сумке Вы находите: ', ", ".join(final_artefacts), '''
+* Сохраните на будущее. Вдруг пригодится.''')
     elif i == '1' and final_artefacts == []:
-        print('Это не сумка тяжелая, а вы устали.')
+        print('Это не сумка тяжелая, а Вы устали.')
     else:
         print('Странное решение. Но как хотите.')
     if artefacts_number > 10 or artefacts_number == 10:
@@ -85,4 +90,5 @@ artefacts_number = result_for_final_fight[0]
 #строка good, bad, verybad для определение концовки
 artefacts_result = result_for_final_fight[1]
 
+sc.ending_scene(Boss, player, artefacts_result)
 # Настя! здесь нужно написать и вызвать функцию битвы с боссом. используй artefacts_result, чтобы узнать об артефактах
